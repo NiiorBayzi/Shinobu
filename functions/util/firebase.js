61738e65ff4ac7ea8aa8e50a62d99ff94a88309a
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-19 13:10:34 -0300

    Update firebase.js

--- functions/util/firebase.js
+++ functions/util/firebase.js
@@ -1,4 +1,6 @@
 const FirebaseUtil = require("firebase-util.js");
+const mongoose = require("mongoose");
+
 const dbF = new FirebaseUtil({
   apiKey: "AIzaSyDPDNUfk1IIlcphWfcw-PhLzuNx1N0TQTI",
   authDomain: "ryomen-sukuna.firebaseapp.com",
@@ -9,5 +11,10 @@ const dbF = new FirebaseUtil({
   appId: "1:314777611908:web:a3c3dfa1ef15dac0075cd1",
   measurementId: "G-F2H4QN02GS"
 });
+
+async function connectDatabase() {
+  const connection = await mongoose.connect(process.env.MONGO_URL);
+  client.db = { connection };
+}
 
 global.db = dbF;
