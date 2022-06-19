9b3e80c3427b5b996a465157f9a136f017159052
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-19 14:12:28 -0300

    Update global.js

--- functions/util/global.js
+++ functions/util/global.js
@@ -5,8 +5,8 @@ module.exports = async () => {
   let result = cts.map((c) => {
     let pib = 0;
     c.members.map(async (u) => { 
-      let cash = await db.get(`users/${u}/cash`);
-      pib = pib + cash;
+      let cash = await db.get(`users/${u}/cash`) || 0;
+      pib = pib + Number(cash);
     });
     c.pib = pib;
     c.index = i;
