2359c36546250193a3abeefb8377e06caa82575b
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-18 23:53:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ client.on("interactionCreate", async (interaction) => {
   if (!interaction.isSelectMenu()) return;
 
   if (interaction.customId == `works-${interaction.user.id}`) {
-    let xp = db.get(`users/${interaction.user.id}/xp`) || 0;
+    let xp = await db.get(`users/${interaction.user.id}/xp`) || 0;
     let select = interaction.values[0].split("/");
     if (xp < select[1]) return interaction.reply({ content: `<:_:986860739920814100> | Você precisa usar pelo menos **${select[1]} EXP** para essa profissão.`, ephemeral: true });
 
@@ -50,7 +50,7 @@ client.on("interactionCreate", async (interaction) => {
     if (vl === "eco") {
       let arr = [];
       let i = 1;
-      Object.entries(db.get("users")).sort((a, b) => b.cash - a.cash).map((lb) => {
+      Object.entries(await db.get("users")).sort((a, b) => b.cash - a.cash).map((lb) => {
         if (isNaN(lb[1].cash)) return;
         if (!client.users.cache.get(lb[0])) return;
         arr.push(`\`[ ${i++}° ]\` **${parseText(client.users.cache.get(lb[0]).tag)}** - $${lb[1].cash.toLocaleString()}`);
