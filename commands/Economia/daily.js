19d2878f00df374d4e08de46fb7b2edf51b8768b
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-18 23:47:01 -0300

    Update daily.js

--- commands/Economia/daily.js
+++ commands/Economia/daily.js
@@ -3,12 +3,13 @@ module.exports = {
   category: "Economia",
   aliases: ["diária", "diaria"],
   run: async (client, message, args) => {
-    if ((db.get(`users/${message.author.id}/cooldowns/daily`) || 0) > Date.now()) return message.reply(`<:cooldown:986467656058171422> | Você precisa esperar **\`${parseTime(db.get(`users.${message.author.id}.cds.daily`) - Date.now())}\`** para coletar sua recompensa diária novamente.`);
+    let cd = await db.get(`users/${message.author.id}/cooldowns/daily`);
+    if ((cd || 0) > Date.now()) return message.reply(`<:cooldown:986467656058171422> | Você precisa esperar **\`${parseTime(cd - Date.now())}\`** para coletar sua recompensa diária novamente.`);
 
     let won = randomNumber(528, 1056);
     message.reply(`<:_:986398385080778812> | **${parseText(message.author.username)}**, você fez sua coleta diária e ganhou **$${won}**, volte em <t:${Math.round((Date.now() + 86400000) / 1000)}:R> para coletar novamente.`);
 
-    db.add(`users/${message.author.id}/cash`, won);
+    db.math(`users/${message.author.id}/cash`, "+", won);
     db.set(`users/${message.author.id}/cooldowns/daily`, Date.now() + 86400000);
   }
 }
