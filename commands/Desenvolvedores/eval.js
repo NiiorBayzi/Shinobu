211b162a7b20b2ae5bcdb0b10c33e35ff73322a5
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-19 00:47:16 -0300

    Update eval.js

--- commands/Desenvolvedores/eval.js
+++ commands/Desenvolvedores/eval.js
@@ -6,10 +6,20 @@ module.exports = {
   aliases: ["ev", "e"],
   category: "Desenvolvedores",
   run: async (client, message, args) => {
-    if (!client.config.devs.includes(message.author.id)) return message.reply("**<:_:986835551372075068> | Você não é um de meus desenvolvedores.**");
+    if (!client.config.devs.includes(message.author.id)) return message.reply("**[ <:_:986835551372075068> ] | Você não é um de meus desenvolvedores.**");
     try {
       let code = args.join(" ");
-      let res = await require("util").inspect(eval(code));
+      let res = "";
+      if (args[0] === "--o ") {
+        code = code.replace("--o ", "");
+        global.main = client;
+        global.main.message = message;
+        global.db = db
+        global.main.message.args = args;
+        res = require('util').inspect(await Object.getPrototypeOf(async () => { }).constructor(code)())
+      } else {
+        res = await require("util").inspect(eval(code));
+      }
 
       message.channel.send(`\`\`\`js\n${res.slice(0, 1990).replaceAll(client.token, "/* Token */")}\`\`\``)
     } catch (err) {
