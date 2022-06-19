cb997193d7ad2d4d52da667a540a745355220041
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-18 22:00:13 -0300

    Update work.js

--- commands/Economia/work.js
+++ commands/Economia/work.js
@@ -9,7 +9,7 @@ module.exports = {
     if ((db.get(`users.${message.author.id}.cds.work`) || 0) > Date.now()) return message.reply(`<:_:986467656058171422> | Você precisa esperar **\`${parseTime(db.get(`users.${message.author.id}.cds.work`))}\`** para trabalhar novamente.`);
     let tm = randomNumber(4, 8);
     let won = randomNumber(prL.min, prL.max) * tm;
-    message.reply(`<:_:986398385080778812> | **${parseText(message.author.username)}**, você trabalhou por **${tm} Horas** e ganhou **$${won}**, volte em <t:${Math.round(Date.now() / 1000 + 54000)}:R> para trabalhar novamente.`);
+    message.reply(`<:_:986398385080778812> | **${parseText(message.author.username)}**, você trabalhou por **${tm} Horas** e ganhou **$${won}**, volte em <t:${Math.floor((Date.now() + 5400000) / 1000)}:R> para trabalhar novamente.`);
 
     db.add(`users.${message.author.id}.cash`, won);
     db.set(`users.${message.author.id}.cds.work`, Date.now() + 5400000);
