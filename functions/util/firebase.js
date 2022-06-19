f49759e7c1344dd93be52b69307090c89b0e70d6
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-18 22:06:42 -0300

    Create firebase.js

---
+++ functions/util/firebase.js
@@ -0,0 +1,12 @@
+const db = require("firebase-util.js")({
+  apiKey: "AIzaSyDPDNUfk1IIlcphWfcw-PhLzuNx1N0TQTI",
+  authDomain: "ryomen-sukuna.firebaseapp.com",
+  databaseURL: "https://ryomen-sukuna-default-rtdb.firebaseio.com",
+  projectId: "ryomen-sukuna",
+  storageBucket: "ryomen-sukuna.appspot.com",
+  messagingSenderId: "314777611908",
+  appId: "1:314777611908:web:a3c3dfa1ef15dac0075cd1",
+  measurementId: "G-F2H4QN02GS"
+});
+
+global.db = db;
