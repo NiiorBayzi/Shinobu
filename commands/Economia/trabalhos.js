c7d74740392daa4c5759186288c64231c66a97c2
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-18 22:25:18 -0300

    Update trabalhos.js

--- commands/Economia/trabalhos.js
+++ commands/Economia/trabalhos.js
@@ -2,12 +2,12 @@ module.exports = {
   name: "trabalhos",
   category: "Economia",
   run: async (client, message, args) => {
-    let xp = db.get(`users.${message.author.id}.xp`) || 0;
+    let xp = db.get(`users/${message.author.id}/xp`) || 0;
 
     let embed = new Discord.MessageEmbed()
       .setColor(client.config.color)
       .setTitle("<:_:986402052957696061> | Trabalhos")
-      .setDescription(`Escolha a profissão que você deseja se tornar no menu abaixo.\n> **XP:** \`${xp}\``)
+      .setDescription(`Escolha a profissão que você deseja se tornar no menu abaixo.\n> **EXP:** \`${xp}\``)
       .setFooter({ text: `${message.author.tag} (${message.author.id})`, iconURL: message.author.avatarURL({ dynamic: true }) });
     let arr = [];
     Object.entries(require("../../economy.json")).forEach(t => {
