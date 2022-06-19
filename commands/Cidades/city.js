bddd623e82f7ac789b71107bab429f737d5c39eb
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-19 11:08:48 -0300

    Update city.js

--- commands/Cidades/city.js
+++ commands/Cidades/city.js
@@ -24,12 +24,15 @@ module.exports = {
       if (!city) return message.reply(`<:_:987355647987318834> | **${parseText(user.username)}** não está em uma cidade ainda.`);
 
       let members = [];
-      let m = await db.get(`cities/${city.index}/members`);
-      city.members.map(u => client.users.findUser(u) != null ? members.push(`**${parseText(client.users.findUser(u).tag)}**`) : () => { db.set(`cities/${city.index}/members`, m.splice(m.indexOf(u))); city.members = m.splice(m.indexOf(u)); })
+      city.members.map(u => {
+        let us = client.users.findUser(u);
+        if (!us) return;
+        members.push(`**${parseText(client.users.findUser(u).tag)}**`)
+      })
       let embed = new Discord.MessageEmbed()
         .setColor(client.config.color)
         .setTitle(`<:_:987363119837872138> | Cidade de ${user.username}`)
-        .addFields({ name: "<:_:987355647987318834> » Informações:", value: `>>> **Nome:** ${city.name}\n**Dono:** ${parseText(client.users.findUser(city.members[0]))}` }, { name: `<:_:987362684565618829> » PIB:`, value: `> PIB de **$${city.pib.toLocaleString()}**` }, { name: `<:_:987362339969990707> » Habitantes: [${city.members.length}]`, value: `>>> ${members.join("\n")}` })
+        .addFields({ name: "<:_:987355647987318834> » Informações:", value: `>>> **Nome:** ${city.name}\n**Dono:** ${parseText(client.users.findUser(members[0]))}` }, { name: `<:_:987362684565618829> » PIB:`, value: `> PIB de **$${city.pib.toLocaleString()}**` }, { name: `<:_:987362339969990707> » Habitantes: [${city.members.length}]`, value: `>>> ${members.join("\n")}` })
         .setThumbnail(user.avatarURL({ dynamic: true }))
 
       message.reply({ embeds: [embed] });
