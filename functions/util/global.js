6a0afd881ed20ae78e17962c29554dbe85cc38bf
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-19 12:06:57 -0300

    Update global.js

--- functions/util/global.js
+++ functions/util/global.js
@@ -2,7 +2,7 @@ module.exports = async () => {
   let cts = await db.get("cities");
   if (!cts) cts = [];
   let i = 0;
-  let result = cts.map(async (c) => {
+  let result = cts.map((c) => {
     let pib = 0;
     c.members.map(async (u) => { 
       let cash = await db.get(`users/${u}/cash`);
@@ -13,5 +13,5 @@ module.exports = async () => {
     return [i++, c];
   });
 
-  global.cities = new Discord.Collection(await result);
+  global.cities = new Discord.Collection(result);
 }
