eff310dac603f928473e2d20e0993cb063e85e58
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-20 19:30:00 -0300

    Update botinfo.js

--- commands/Informações/botinfo.js
+++ commands/Informações/botinfo.js
@@ -5,11 +5,11 @@ module.exports = {
     let embed = new Discord.MessageEmbed()
       .setColor(client.config.color)
       .setAuthor({ name: client.user.username, iconURL: client.user.avatarURL() })
-      .setThumbnail(message.author.avatarURL({ dynamic: true })
+      .setThumbnail(message.author.avatarURL({ dynamic: true }))
       .setDescription(`Olá **${parseText(message.author.username)}**, eu sou o **${client.user.username}**, um bot de **Economia de Cidades**`)
       .addFields({
         name: `(${emoji.bot}) » Informações`,
-        value: `>>> Criador: ${client.users.cache.get("916712541797896263").tag} \`(916712541797896263)\``
+        value: `>>> **Criador:** ${client.users.cache.get("916712541797896263").tag} \`(916712541797896263)\``
       }, {
         name: `(${emoji.statistics}) » Estatísticas`,
         value: `>>> **Servidores:** ${client.guilds.size.toLocaleString("pt-br")}\n**Usuários:** ${client.users.size.toLocaleString("pt-br")}\n**Comandos:** ${client.commands.size.toLocaleString("pt-br")}`
