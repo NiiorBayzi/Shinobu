91ada99d08301cce17a808af6ba67b16900bacdb
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-20 17:52:54 -0300

    Update ping.js

--- commands/Informações/ping.js
+++ commands/Informações/ping.js
@@ -7,6 +7,6 @@ module.exports = {
     await client.db.users.findOneAndUpdate({ _id: client.user.id }, { $set: { _p: 1 } })
     mongoPing = Date.now() - now;
 
-    message.reply(`**(${emoji.ping}) | Pong!**\n>>> **(${emoji.gateway}) Gateway: \`${client.ws.ping}ms\`\n(${emoji.database}) Database: \`${mongoPing}ms\`**`);
+    message.reply(`**(${emoji.ping}) | Pong!**\n>>> **(${emoji.gateway}) Gateway: \`${client.ws.ping}ms\`\n(${emoji.database}) Database: \`${mongoPing}ms\`\n(${emoji.time}) Tempo de resposta: \`${Date.now() - message.createdTimestamp}ms\`**`);
   }
 }
