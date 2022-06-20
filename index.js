dcf9e572c9bd9b7b55ad75377e7d25c9e11a4088
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-19 21:40:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 global.Discord = require("discord.js");
 global.glob = require("glob");
+const jssearch = require("js-search");
+global.search = new jssearch.Search('isbn');
+search.indexStrategy = new jssearch.AllSubstringsIndexStrategy();
 const { Client, Collection, MessageEmbed } = Discord;
 global.client = new Client({ intents: 32767 });
 client.commands = new Collection();
 
-client.on("ready", () => {
+client.once("ready", () => {
   console.log(`[ Discord.js ] Successfully connected in ${client.user.tag}.`);
 });
 
@@ -14,6 +17,36 @@ glob.sync("./commands/**/*.js").forEach(f => {
   command.category = dir[dir.length - 2];
 
   client.commands.set(command.name, command);
+});
+
+client.on("messageCreate", async (message) => {
+  if (!message.author.bot) return;
+  const cooldown = new Collection();
+  let prefix = "sk?";
+  
+  if (message.content.toLowerCase().startsWith(prefix)) {
+    if (cooldown.get(message.author.id)) return;
+    let args = message.content.trim().slice(prefix.length).split(/ +/g);
+    let command = args.shift().toLowerCase();
+    if (!command) return;
+    let cmd = client.commands.get(command) || client.users.cache.find(c => (x.aliases || []).includes(command));
+    if (!cmd) {
+      search.addDocuments(client.commands.filter(x => (x.dev || false) != true);
+      search.addIndex('name');
+      search.addIndex('aliases');
+      let sh = search.search(command)[0];
+      return message.reply(`Não encontrei este comando${sh ? `, você quis dizer \`${sh.name}\`?` : ", verifique como você colocou."}`);
+    }
+
+    try {
+      cmd.run(client, message, args);
+      
+      cooldown.set(message.author.id, true);
+      setCooldown(() => cooldown.delete(message.author.id), 4000)
+    } catch (err) {
+      
+    }
+  }
 });
 
 client.login(process.env.token);
