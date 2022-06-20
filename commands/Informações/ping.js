bca568a513f8b9fc2ae1ef252a4cf90d69314511
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-20 17:24:13 -0300

    Update ping.js

--- commands/Informações/ping.js
+++ commands/Informações/ping.js
@@ -2,12 +2,12 @@ module.exports = {
   name: "ping",
   aliases: ["latency", "latência"],
   run: async (client, message, args) => {
-    let mongoPing = 1;
+    let mongoPing = 100;
     let now = Date.now();
     client.db.users.findOneAndUpdate({ _id: client.user.id }, { $set: { ping: 1 } }).then((a) => {
       mongoPing = Date.now() - now;
     });
 
-    message.reply(`**(${emoji.ping}) | Pong!**\n>>> **(${emoji.gateway}) Gateway: \`${client.ws.ping}ms\`\n(${emoji.database}) Database: ${mongoPing}**`);
+    message.reply(`**(${emoji.ping}) | Pong!**\n>>> **(${emoji.gateway}) Gateway: \`${client.ws.ping}ms\`\n(${emoji.database}) Database: ${mongoPing}ms**`);
   }
 }
