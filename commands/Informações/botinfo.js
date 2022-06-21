1f4f32c2350827d899483fa7c1a5eee0119eeb5a
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-20 22:10:01 -0300

    Update botinfo.js

--- commands/Informações/botinfo.js
+++ commands/Informações/botinfo.js
@@ -9,10 +9,13 @@ module.exports = {
       .setDescription(`Olá **${parseText(message.author.username)}**, eu sou o **${client.user.username}**, um bot de **Economia de Cidades**`)
       .addFields({
         name: `(${emoji.bot}) » Informações`,
-        value: `>>> **Criador:** ${client.users.cache.get("916712541797896263").tag} \`(916712541797896263)\``
+        value: `>>> **Criador:** ${client.users.cache.get("916712541797896263").tag} \`(916712541797896263)\`\n**Criado em:** <t:${Date. parse(client.user.createdAt) / 1000}:F>`
       }, {
         name: `(${emoji.statistics}) » Estatísticas`,
         value: `>>> **Servidores:** ${client.guilds.cache.size.toLocaleString("pt-br")}\n**Usuários:** ${client.users.cache.size.toLocaleString("pt-br")}\n**Comandos:** ${client.commands.size.toLocaleString("pt-br")}`
+      }, {
+        name: `(${emoji.host}) » Sistema`,
+        value: `>>> `
       })
       .setFooter({ text: message.author.tag, iconURL: message.author.avatarURL() })
       .setTimestamp()
