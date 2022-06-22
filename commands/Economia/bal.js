ef7475ff80d197d2bd68b740989746a59bbc0dc7
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-21 23:30:23 -0300

    Update bal.js

--- commands/Economia/bal.js
+++ commands/Economia/bal.js
@@ -4,7 +4,7 @@ module.exports = {
   run: async (client, message, args, db) => {
     let user = client.users.findUser(args.join(" ")) || message.author;
     let data = db.find(x => x._id === user.id);
-    if (!data) message.reply(`**(${emoji.error}) | ${parseText(message.author.username)}**, não encontrei esse usuário em meu **(${emoji.database}) Banco de Dados**.`);
+    if (!data) return message.reply(`**(${emoji.error}) | ${parseText(message.author.username)}**, não encontrei esse usuário em meu **(${emoji.database}) Banco de Dados**.`);
 
     message.reply(`**(${emoji.wallet}) | <@${message.author.id}>**, **${parseText(user.username)}** possui **${emoji.starcoins} ${data.cash}** eu sua carteira.`);
   }
