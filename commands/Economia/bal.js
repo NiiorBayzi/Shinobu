ca120673f9caed2f270dc2e6386a16544ad3192c
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-22 10:36:06 -0300

    Update bal.js

--- commands/Economia/bal.js
+++ commands/Economia/bal.js
@@ -5,8 +5,8 @@ module.exports = {
     let user = client.users.findUser(args.join(" ")) || message.author;
     let data = db.find(x => x._id === user.id);
     if (!data) return message.reply(`**(${emoji.error}) | ${parseText(message.author.username)}**, não encontrei esse usuário em meu **(${emoji.database}) Banco de Dados**.`);
-    let pos = db.filter((a) => a.cash > 0).sort((a, b) => b.cash - a.cash).findIndex(x => x._id === message.author.id) + 1;
+    let pos = db.filter((a) => a.cash > 0).sort((a, b) => b.cash - a.cash).findIndex(x => x._id === user.id) + 1;
 
-    message.reply(`**(${emoji.wallet}) | <@${message.author.id}>**, **${parseText(user.username)}** possui **${emoji.starcoins} ${data.cash}** eu sua carteira${pos > 0 ? `.\n> **(${emoji.rank}) |** Sabia que **${parseText(message.author.username)}** está na posição **#${pos}** no ranking?` : "."}`);
+    message.reply(`**(${emoji.wallet}) | <@${message.author.id}>**, **${parseText(user.username)}** possui **${emoji.starcoins} ${data.cash}** eu sua carteira${pos > 0 ? `.\n> **(${emoji.rank}) |** Sabia que **${parseText(user.username)}** está na posição **#${pos}** no ranking?` : "."}`);
   }
 }
