2f21b00298b0779877137cf7b014968d1a7303c6
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-21 22:45:58 -0300

    Update bal.js

--- commands/Economia/bal.js
+++ commands/Economia/bal.js
@@ -2,6 +2,9 @@ module.exports = {
   name: "bal",
   aliases: ["atm", "wallet", "saldo", "carteira],
   run: async (client, message, args) => {
-    let user = "";
+    let user = client.users.findUser(args.join(" ")) || message.author;
+    let cash = client.db.users.findOne({ _id: message.author.id }).cash;
+
+    message.reply(`**(${emoji.wallet}) »** <@${message.author.id}>, **${parseText(user.username)}** possui **${emoji.stars} ${cash}** eu sua carteira.`);
   }
 }
