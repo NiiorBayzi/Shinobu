f57e1868465c935609997d20b88a785950582898
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-23 10:56:17 -0300

    Update termo.js

--- commands/Jogos/termo.js
+++ commands/Jogos/termo.js
@@ -1,4 +1,5 @@
 const termo = require("../../games/termo/index.js");
+const cd = new Discord.Collection();
 
 module.exports = {
   name: "termo",
@@ -23,14 +24,16 @@ module.exports = {
       const collector = message.channel.createMessageCollector({ filter, time: 90000 });
       
       collector.on("collect", (m) => {
+        if (cd.get(m.author.id)) return message.reply(`**(${emoji.time}) | ${parseText(m.author.username)}**, você está digitando muito rápido.`).then((a) => { setTimeout(() => a.delete(), 2500) });
+        cd.set(m.author.id, true);
+        setTimeout(() => db.delete(m.author.id), 1500);
         if (["tip", "dica", "tips", "dicas"].includes(m.content)) {
           let tipArr = word.slice(0, word.length / 2);
           word.slice(word.length / 2, word.length).forEach(x => tipArr.push("_"));
-
           return m.reply(`**(${emoji.tip}) | Dica:** \`${tipArr.join(" ")}\`.`)
         }
         answer = m.content.trim().split(/ +/g);
-        created = termo.add(parseText(answer[0].toLowerCase()), message.author.id);
+        created = termo.add(parseText(answer[0].toLowerCase()), m.author.id);
 
         embed.setDescription(`${created.attempts.join("\n")}`)
         msg.edit({ embeds: [embed] });
@@ -40,12 +43,11 @@ module.exports = {
       });
       
       collector.on("end", (collected, reason) => {
+        termo.stop(message.author.id);
         if (reason == "time") {
           msg.reply(`**(${emoji.time}) | <@${message.author.id}>**, seu tempo acabou, a palavra era \`${created.word}\`.`);
-          termo.stop(message.author.id);
         } else if (reason == "wins") {
           msg.reply(`**(${emoji.confetti}) | <@${message.author.id}>**, você conseguiu acertar a palavra do termo.`);
-          termo.stop(message.author.id);
         }
       });
     });
