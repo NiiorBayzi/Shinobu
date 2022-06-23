240a8c8d9b8515135611dc8947b968e87bf81d80
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-23 00:11:53 -0300

    Update termo.js

--- commands/Jogos/termo.js
+++ commands/Jogos/termo.js
@@ -6,6 +6,14 @@ module.exports = {
     let created = termo.create(args[0], message.author.id);
     if (!created) return message.reply(`**(${emoji.error}) | ${parseText(message.author.username)}**, você já está em uma partida.`);
 
-    
+    let embed = new Discord.MessageEmbed()
+      .setColor(client.config.color)
+      .setTitle(`(${emoji.graduation}) | Termo`)
+      .setDescription(`${created.attempts.join("\n")}`)
+      .setThumbnail("https://cdn.discordapp.com/emojis/989366183235043378.png?size=2048")
+      .setFooter({ text: `${message.author.tag} (${message.author.id})`, iconURL: message.author.avatarURL({ dynamic: true }) })
+      .setTimestamp()
+
+    message.reply({ embeds: [embed] });
   }
 }
