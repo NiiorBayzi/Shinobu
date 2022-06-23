f87172dde2bd8793c6fe73081bedc3e720408669
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-22 22:44:55 -0300

    Update bal.js

--- commands/Economia/bal.js
+++ commands/Economia/bal.js
@@ -7,6 +7,6 @@ module.exports = {
     if (!data) return message.reply(`**(${emoji.error}) | ${parseText(message.author.username)}**, não encontrei esse usuário em meu **(${emoji.database}) Banco de Dados**.`);
     let pos = db.filter((a) => a.cash > 0).sort((a, b) => b.cash - a.cash).findIndex(x => x._id === user.id) + 1;
 
-    message.reply(`**(${emoji.wallet}) | <@${message.author.id}>**, **${parseText(user.username)}** possui **${emoji.starcoins} ${data.cash}** eu sua carteira${pos > 0 ? `.\n> **(${emoji.rank}) |** Sabia que **${parseText(user.username)}** está na posição **#${pos}** no ranking?` : "."}`);
+    message.reply(`**(${emoji.wallet}) | <@${message.author.id}>**, **${parseText(user.username)}** possui **${emoji.starcoins} ${data.cash}** sua carteira${pos > 0 ? `.\n> **(${emoji.rank}) |** Sabia que **${parseText(user.username)}** está na posição **#${pos}** no ranking?` : "."}`);
   }
 }
