a3b49eaeeb8ab79142edf80c885b32feb6e677e2
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-23 23:00:51 -0300

    Update help.js

--- commands/Informações/help.js
+++ commands/Informações/help.js
@@ -5,11 +5,16 @@ module.exports = {
     let embed = new Discord.MessageEmbed()
       .setColor(client.config.color)
       .setTitle(`Painel de Ajuda | ${client.user.username}`)
-      .setThumbnail()
+      .setThumbnail(emoji.attendant)
       .addFields({
         name: `${emoji.wallet} » Economia`,
         value: "`" + client.commands.filter(x => !x.dev && x.category == "Economia").map(x => x.name).join("` | `") + "`"
+      }, {
+        name: `${emoji.info} » Informações`,
+        value: "`" + client.commands.filter(x => !x.dev && x.category == "Informações").map(x => x.name).join("` | `") + "`"
       })
+      .setFooter({ text: `${message.author.tag} (${message.author.id})`, iconURL: client.user.avatarURL({ dynamic: true }) })
+      .setTimestamp()
 
     message.reply({ embeds: [embed] });
   }
