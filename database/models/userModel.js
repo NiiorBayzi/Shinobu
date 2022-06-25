f46f9168c8759791f13aaa5ff75a03f995ee456b
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-25 22:15:53 +0000

    Update userModel.js

--- database/models/userModel.js
+++ database/models/userModel.js
@@ -9,7 +9,7 @@ const UserSchema = new mongoose.Schema({
     weekly: { type: Number }
   },
   media: {
-    at: { type: String, required: true },
+    at: { type: String },
     bio: { type: String },
     posts: { type: Array }
   },
