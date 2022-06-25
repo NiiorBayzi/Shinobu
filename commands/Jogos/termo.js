96904743c6e295269f58b9d38dd8a69cd1a01811
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-25 00:29:42 -0300

    Update termo.js

--- commands/Jogos/termo.js
+++ commands/Jogos/termo.js
@@ -4,6 +4,7 @@ const cd = new Discord.Collection();
 module.exports = {
   name: "termo",
   aliases: ["term"],
+  dev: true,
   run: async (client, message, args) => {
     let created = termo.create("started", message.author.id);
     if (!created) return message.reply(`**(${emoji.error}) | ${parseText(message.author.username)}**, você já está em uma partida.`);
@@ -25,14 +26,14 @@ module.exports = {
         if (cd.get(m.author.id)) return message.reply(`**(${emoji.time}) | ${parseText(m.author.username)}**, você está digitando muito rápido.`).then((a) => { setTimeout(() => a.delete(), 2500) });
         cd.set(m.author.id, true);
         setTimeout(() => cd.delete(m.author.id), 1500);
-        answer = parseText(m.content.trim()).split(/ +/g);
-        if (answer.length < 5) return message.reply(`**(${emoji.error}) | ${parseText(m.author.username)}**, coloque no mínimo **5 letras**.`).then((a) => { setTimeout(() => a.delete(), 2500) });
+        answer = m.content.trim().split(/ +/g);
+        if (parseText(answer[0]) < 5) return message.reply(`**(${emoji.error}) | ${parseText(m.author.username)}**, coloque no mínimo **5 letras**.`).then((a) => { setTimeout(() => a.delete(), 2500) });
         if (["tip", "dica", "tips", "dicas"].includes(m.content.toLowerCase())) {
           let tipArr = word.slice(0, word.length / 2);
           word.slice(word.length / 2, word.length).forEach(x => tipArr.push("_"));
           return m.reply(`**(${emoji.tip}) | Dica:** \`${tipArr.join(" ")}\`.`)
         }
-        created = termo.add(answer[0].toLowerCase(), m.author.id);
+        created = termo.add(parseText(answer[0].toLowerCase()), m.author.id);
         embed.setDescription(`${(created.attempts.length > 10 ? created.attempts.slice(created.attempts.length - 10, created.attempts.length) : created.attempts).join("\n")}`)
         msg.edit({ embeds: [embed] });
         if (created.win) {
