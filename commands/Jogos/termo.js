70737f0321b1bef442299a932ca91bebc50b61a8
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-06-25 00:15:42 -0300

    Update termo.js

--- commands/Jogos/termo.js
+++ commands/Jogos/termo.js
@@ -5,16 +5,14 @@ module.exports = {
   name: "termo",
   aliases: ["term"],
   run: async (client, message, args) => {
-    let first = parseText(args[0]?.toLowerCase());
-    if (!first) return message.reply(`**(${emoji.error}) | ${parseText(message.author.username)}**, digite a primeira palavra para começar.`);
-    let created = termo.create(first, message.author.id);
+    let created = false;
     if (!created) return message.reply(`**(${emoji.error}) | ${parseText(message.author.username)}**, você já está em uma partida.`);
-    let word = created.word.split("");
+    let word;
 
     let embed = new Discord.MessageEmbed()
       .setColor(client.config.color)
       .setTitle(`(${emoji.graduation}) | Termo`)
-      .setDescription(`${created.attempts.slice(0).join("\n")}`)
+      .setDescription(`Envie a primeira palavra para começar o termo.`)
       .setThumbnail("https://cdn.discordapp.com/emojis/989366183235043378.png?size=2048")
       .setFooter({ text: `${message.author.tag} (${message.author.id})`, iconURL: message.author.avatarURL({ dynamic: true }) })
       .setTimestamp()
@@ -26,15 +24,19 @@ module.exports = {
       collector.on("collect", (m) => {
         if (cd.get(m.author.id)) return message.reply(`**(${emoji.time}) | ${parseText(m.author.username)}**, você está digitando muito rápido.`).then((a) => { setTimeout(() => a.delete(), 2500) });
         cd.set(m.author.id, true);
-        setTimeout(() => cd.delete(m.author.id), 1500);
-        if (["tip", "dica", "tips", "dicas"].includes(m.content.toLowerCase())) {
-          let tipArr = word.slice(0, word.length / 2);
-          word.slice(word.length / 2, word.length).forEach(x => tipArr.push("_"));
-          return m.reply(`**(${emoji.tip}) | Dica:** \`${tipArr.join(" ")}\`.`)
+        answer = parseText(m.content.trim()).split(/ +/g);
+        if (answer.length < 4) return message.reply(`**(${emoji.error}) | ${parseText(m.author.username)}**, coloque no mínimo **5 letras**.`).then((a) => { setTimeout(() => a.delete(), 2500) });
+        if (!termo.info(message.author.id)) {
+          created = termo.create(answer[0].toLowerCase(), m.author.id);
+        } else {
+          setTimeout(() => cd.delete(m.author.id), 1500);
+          if (["tip", "dica", "tips", "dicas"].includes(m.content.toLowerCase())) {
+            let tipArr = word.slice(0, word.length / 2);
+            word.slice(word.length / 2, word.length).forEach(x => tipArr.push("_"));
+            return m.reply(`**(${emoji.tip}) | Dica:** \`${tipArr.join(" ")}\`.`)
+          }
+          created = termo.add(answer[0].toLowerCase(), m.author.id);
         }
-        answer = m.content.trim().split(/ +/g);
-        created = termo.add(parseText(answer[0].toLowerCase()), m.author.id);
-
         embed.setDescription(`${(created.attempts.length > 10 ? created.attempts.slice(created.attempts.length - 10, created.attempts.length) : created.attempts).join("\n")}`)
         msg.edit({ embeds: [embed] });
         if (created.win) {
